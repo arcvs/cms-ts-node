7460f7710ab6c88ef5a461d8699d6ac4c4c97ee1
Vladimir <arctic.vs@gmail.com>
2025-12-02 19:00:15 +0300

    Поехали

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express, {type Application, type Request, type Response } from 'express';
+
+const app: Application = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, TypeScript + Express!!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+//.filter(file => file.endsWith(".js"))
---
+++ tstIndexFiles.js
@@ -0,0 +1,13 @@
+import { readdir } from 'node:fs/promises';
+
+async function main() {
+    try {
+        const files = await readdir("./");
+        for (const file of files)
+            console.log(file);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+main()
