d35385e9b44cf20c0eab30a8c5d84d5afb4b927e
Vladimir <arctic.vs@gmail.com>
2025-12-08 07:52:51 +0300

    add class handler API

--- src/components/articles/articles-api.ts
+++ src/components/articles/articles-api.ts
@@ -14,8 +14,11 @@ router.get('/', getItems);
 
 router.get('/:id', (req, res, next) => handlerRequest);
 
+// router.get('/:ids', handlerRequestToRead);
 
 router.post('/', createItem);
+
+
 router.put('/:id', updateItem);
 router.delete('/:id', deleteItem);
 
@@ -37,17 +40,61 @@ type article = {
   name: string
 }
 
+
+
+const handlerRequestToRead = (path: string, contoller: any) => {
+  router.get(path, (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(404).json({ message: 'Item not found' });
+      res.json(Controller());
+    } catch (error) {
+      next(error);
+    }
+  });
+}
+
+handlerRequestToRead('/:id', Controller)
+
+
+
+
 function handlerRequest(req: Request, res: Response, next: NextFunction) {
   try {
-    // res.status(404).json({ message: 'Item not found' });
-    res.json(controller());
+    res.status(404).json({ message: 'Item not found' });
+    res.json(Controller());
   } catch (error) {
     next(error);
   }
 }
 
-function controller(params?: article[]) {
+function Controller(params?: article[]) {
   const item: article = { id: 1, name: 'John1' }
+
   console.log(item);
   return item
 }
+
+
+class HandlerAPI {
+
+  constructor(private router: Router) {}
+
+  requestToRead(path: string, contoller: any) {
+    this.router.get('/', getItems)
+
+    return this.router
+  }
+
+
+  requestToCreate(path: string, contoller: any) {
+
+  }
+  requestToUpdate(path: string, contoller: any) {
+
+  }
+  requestToDelete(path: string, contoller: any) {}
+}
+
+
+const api = new HandlerAPI(Router())
+console.log(api);
